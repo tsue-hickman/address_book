401769bf40df4d1176ccd95a7836e1631d493613
Tsue_Hic22009 <hic22009@byui.edu>
2025-11-04 18:06:23 -0600

    upgraded styling for the site

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,50 @@
 class ContactsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+
   def index
+    @contacts = current_user.contacts.order(last_name: :asc)
   end
 
   def show
   end
 
   def new
+    @contact = current_user.contacts.build
+  end
+
+  def create
+    @contact = current_user.contacts.build(contact_params)
+    if @contact.save
+      redirect_to @contact, notice: "Contact created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_to @contact, notice: "Contact updated successfully!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact.destroy
+    redirect_to contacts_path, notice: "Contact deleted!"
+  end
+
+  private
+
+  def set_contact
+    @contact = current_user.contacts.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :phone)
   end
 end
